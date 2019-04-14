08402f625ae349aed5100dae6908b1cc54a33e67
shyamsha <shyamkumar97@live.com>
2019-04-14 16:05:44 +0530

    minlength

--- app/models/category.js
+++ app/models/category.js
@@ -4,7 +4,8 @@ const categorySchema = new Schema({
 	name: {
 		type: String,
 		required: true,
-		unique: true
+		unique: true,
+		minlength: 3
 	}
 });
 const Category = mongoose.model("Category", categorySchema);
